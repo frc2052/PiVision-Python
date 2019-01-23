f9a7015528866c4d2cd470d2071d59ac63c91027
student <info@team2052.com>
2019-01-22 19:13:49 -0600

    showing mask

--- KKVision.py
+++ KKVision.py
@@ -65,8 +65,10 @@ def threshold_video(frame):
     # Convert BGR to HSV
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
     # define range of red in HSV
-    lower_color = np.array([0,220,25])
-    upper_color = np.array([101, 255, 255])
+    #lower_color = np.array([0,220,25])
+    #upper_color = np.array([101, 255, 255])
+    lower_color = np.array([65, 215, 125])
+    upper_color = np.array([100, 255, 255])
     # hold the HSV image to get only red colors
     mask = cv2.inRange(hsv, lower_color, upper_color)
 
@@ -473,6 +475,7 @@ if __name__ == "__main__":
 
 
         threshold = threshold_video(frame)
+        outputStream.putFrame(threshold)
         processed = findContours(frame, threshold)
         # (optional) send some image back to the dashboard
-        outputStream.putFrame(processed)
+        #outputStream.putFrame(processed)
