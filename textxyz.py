7021ff1f68b9e16fdd829ed9cec9349d6bb54ab4
IanOlds <ian.olds@team2052.com>
2019-04-16 20:59:15 -0500

    new

--- textxyz.py
+++ textxyz.py
@@ -41,20 +41,24 @@ front_upper_green = np.array([95, 255, 255])
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # Define object points for a 9x6 grid
-# objp = np.zeros((6*8,3), np.float32)
-# objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
-# objp = objp * 26
-objp = np.array([[15,150,0],[-40,135,0],[0,0,0],[50,15,0],[290,150,0],[255,15,0],[305,0,0],[345,135,0]], dtype=np.float32)
+objp = np.zeros((6*8,3), np.float32)
+objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
+objp = objp * 26
+#objp = np.array([[15,150,0],[-40,135,0],[0,0,0],[50,15,0],[290,150,0],[255,15,0],[305,0,0],[345,135,0]], dtype=np.float32)
 
 #print(objp)
 
-mtx = np.array([[846.45853739 ,   0,         316.32230442],[  0,         847.93343997, 228.07883833],[  0,          0,          1        ]])
-dist = np.array([[-6.68957067e-02, -1.34039967e+00, -7.84647125e-03, -1.59645273e-02,1.05726650e+01]])
+mtx = np.array([[653.90217311,   0,        302.49861937],[  0,         654.67708708, 254.90595383],[  0,          0,          1        ]])
+
+dist = np.array([[ 2.29930082e-01, -3.71583204e+00,  1.35057973e-02, -6.10760675e-03, 1.71232055e+01]])
+
+# mtx = np.array([[846.45853739 ,   0,         316.32230442],[  0,         847.93343997, 228.07883833],[  0,          0,          1        ]])
+# dist = np.array([[-6.68957067e-02, -1.34039967e+00, -7.84647125e-03, -1.59645273e-02,1.05726650e+01]])
 # Arrays to store object points and image points
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-vd = cv2.VideoCapture(0)
+vd = cv2.VideoCapture(1)
 
 def testcam(imgp, img):
     #ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None,None)
@@ -370,41 +374,41 @@ def findTape(contours, image, centerX, centerY):
 while(True):
 
     ret, img = vd.read()
-    #cv2.imshow("Video cap", img)
+    cv2.imshow("Video cap", img)
    
     inp = cv2.waitKey(1)
     
     if inp == 115: #If input is 's'
         break
-    #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    #ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
 
     # If found, add object points and image points
-    #if ret == True:
-        #objpoints.append(objp)
+    if ret == True:
+        objpoints.append(objp)
 
         #Refine image points
-        #cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        #imgpoints.append(corners)
+        cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners)
 
-        # Draw and display the corners
-        #cv2.drawChessboardCorners(img, (8,6), corners, ret)
-        #cv2.imshow('img',img)
-        #cv2.waitKey(500)
-        #print(corners)
-        #testcam(corners, img)
+        #Draw and display the corners
+        cv2.drawChessboardCorners(img, (8,6), corners, ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(500)
+        print(corners)
+        testcam(corners, img)
     
     #undist = cv2.undistort(img, mtx, dist, None, mtx)
     #chicken code
-    boxBlur = blurImg(img, front_green_blur)
-    threshold = threshold_video(front_lower_green, front_upper_green, boxBlur)
-    processed, tarP = findTargets(img, threshold)
-    if(tarP.size >= 8):
-        testcam(tarP, img)
+    # boxBlur = blurImg(img, front_green_blur)
+    # threshold = threshold_video(front_lower_green, front_upper_green, boxBlur)
+    # processed, tarP = findTargets(img, threshold)
+    # if(tarP.size >= 8):
+    #     testcam(tarP, img)
 
-    cv2.imshow("Video processed", processed)
+    #cv2.imshow("Video processed", processed)
         
 
 cv2.destroyAllWindows()
